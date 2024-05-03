e5511b605b4fea3dafb99c745df3fc606c385ced
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-05-03 18:44:46 +0200

    penangf: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: motorola Model: penangf Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_penangf.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_penangf-user \
+    omni_penangf-userdebug \
+    omni_penangf-eng
---
+++ omni_penangf.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from penangf device
+$(call inherit-product, device/motorola/penangf/device.mk)
+
+PRODUCT_DEVICE := penangf
+PRODUCT_NAME := omni_penangf
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := penangf
+PRODUCT_MANUFACTURER := motorola
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vnd_penangf-user 12 THA33.31-26 11 release-keys"
+
+BUILD_FINGERPRINT := None
